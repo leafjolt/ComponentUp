66d184047f663d57ea94348f216f52846db33dfb
ComponentUp <leafjolt@gmail.com>
2018-11-20 13:27:36 -0600

    Update main.js

--- main.js
+++ main.js
@@ -17,5 +17,5 @@ function searchFunction() {
 }
 function clearSearch() {
     var input = document.getElementById('searching');
-    input.value = "";
+    input.setAttribute('value','My default value');
 }
