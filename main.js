a9441c4cc1cd3ec5ff5cc76f35fb09f4944e075c
ComponentUp <leafjolt@gmail.com>
2018-12-02 17:27:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,21 @@ function searchFunction() {
 function clearSearch() {
     var search = document.getElementById('searching');
     search.value = "";
-    searchFunction();
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('searching');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
 }
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
