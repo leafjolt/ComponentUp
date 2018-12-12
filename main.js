3062c4e9d0d0fcae453789dfe27dd4ce1491819a
ComponentUp <leafjolt@gmail.com>
2018-12-11 21:08:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
+searchFunction();
 function searchFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById('searching');
-    filter = input.value.toUpperCase();
+var input,filter,temp,tfilter,tmp2,tfilter2,ii,i,li,ul;
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
+input = document.getElementById('searching');
+filter = input.value.toUpperCase();
+for (i = 0; i < li.length; i++) {
+      li[i].style.display = "";
 }
+  filter=" "+filter;
+  filter+=" ";
+console.log(filter);
+console.log('');
+  tfilter = filter;
+  temp = tfilter.indexOf(' ');
+  tmp2 = tfilter.indexOf(' ',temp+1);
+  for(ii = 0;ii<20&&temp!=-1;ii++){
+    
+      if(temp!=-1){
+        tfilter2=filter.substring(temp,tmp2);
+        tfilter=filter.substring(temp);
+      }
+if(tmp2==-1)
+break;
+for (i = 0; i < li.length; i++) {
+        if (!(li[i].getAttribute('data-search-tags').toUpperCase().indexOf(tfilter2) > -1)) {
+          li[i].style.display = "none";
+        }
+      }
+temp = filter.indexOf(' ',temp+1);
+tmp2 = filter.indexOf(' ',temp+1);
+    }}
 function clearSearch() {
     var search = document.getElementById('searching');
     search.value = "";
