b0a1630bea6cc475ee4aa06cc6d31e6f88c38db6
ComponentUp <leafjolt@gmail.com>
2018-12-11 22:11:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -32,24 +32,19 @@ temp = filter.indexOf(' ',temp+1);
 tmp2 = filter.indexOf(' ',temp+1);
     }}
 function clearSearch() {
-    var search = document.getElementById('searching');
-    search.value = "";
     var input, filter, ul, li, a, i;
     input = document.getElementById('searching');
+    input.value = "";
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName('li');
 
-    // Loop through all list items, and hide those who don't match the search query
+    
     for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+      li[i].style.display = "";
     }
 }
+
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
