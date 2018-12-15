e71dbfa2f43b9b52363c970229d9c18383b425ed
ComponentUp <leafjolt@gmail.com>
2018-12-15 16:27:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function darkmode() {
     for(var ii = 0;ii<images.length;ii++){
     images[ii].style.filter="invert(98%)";
     }
-    document.body.style.background = "black !important";
+    document.body.style.background = "black";
     } else {
     document.getElementById("body").style.filter = "invert(0%)";
     for(var ii = 0;ii<images.length;ii++){
