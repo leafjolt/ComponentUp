0131e635b3edb133b0c9404f3ddf521412553756
ComponentUp <leafjolt@gmail.com>
2018-12-17 16:51:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -56,9 +56,10 @@ function darkmode() {
     for(var ii = 0;ii<images.length;ii++){
     images[ii].style.filter="invert(98%)";
     }
-    document.body.style.background = "black !important";
+    //document.body.style.background = "black !important";
+    document.body.style.animation = "1s linear infinite forceYellow";
     } else {
-    document.getElementById("body").style.animation = "1s linear infinite forceYellow";
+    document.getElementById("body").style.filter = "invert(0%)";
     for(var ii = 0;ii<images.length;ii++){
     images[ii].style.filter="invert(0%)";
     }
